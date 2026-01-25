50ab99150533a7a204e57c7e81ee4ca86b566702
Renato Weliton <elitonnrenato@gmail.com>
2026-01-25 18:13:32 -0300

    nova página de ofertas

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
+const fs = require('fs');
 const { MercadoPagoConfig, Payment } = require('mercadopago');
 const cors = require('cors');
 const CONFIG_PATH = path.join(__dirname, 'config-oferta.json');
