d1853af79ae05ed6d8b0205bf70743d1fe992765
Renato Weliton <elitonnrenato@gmail.com>
2026-01-27 13:32:27 -0300

    novo commit quase pronto

--- index.js
+++ index.js
@@ -208,7 +208,6 @@ function verificarSessao() {
     const authContainer = document.getElementById('header-auth');
     if (nome && authContainer) {
         let btnAdmin = isAdmin ? `<button onclick="location.href='admin.html'" class="bg-purple-600 text-white px-3 py-2 rounded-xl font-bold text-[10px]">👑 Admin</button>` : '';
-        headerAuth.innerHTML = `<button onclick="fazerLogout()" class="suas-classes-aqui">🚪</button>`;
     }
 }
 function logout() { localStorage.clear(); location.href = 'index.html'; }
